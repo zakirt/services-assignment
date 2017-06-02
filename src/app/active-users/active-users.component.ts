31f59fb48da265e28181497cc7d4c9d58f298a13
Zakir Tariverdiev <zakir-ny@hotmail.com>
2017-06-01 22:16:13 -0400

    Removed all input parameetrs and using service to watch for user status changes. (#1)

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import { UserModel } from '../user.model';
 
 @Component({
     selector: 'app-active-users',
     templateUrl: './active-users.component.html',
     styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-    @Input() users: string[];
+export class ActiveUsersComponent implements OnInit {
+    users: UserModel[];
 
     constructor(private usersService: UsersService) { }
+
+    ngOnInit() {
+        this.users = this.usersService.activeUsers;
+
+        this.usersService.userStatusChanged.subscribe((id: number) => {
+            this.users = this.usersService.activeUsers;
+        });
+    }
 
     onSetToInactive(id: number) {
         this.usersService.deactivateUser(id);
